33cb07f0d16a8e1817694df315601cefc532007b
Robert Fischer <robert@programming-with-design.at>
2017-10-19 12:26:24 +0200

    hello world test

---
+++ transition/transition/transition.cpp
@@ -0,0 +1,14 @@
+// transition.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "conio.h"
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+	_getch();
+    return 0;
+}
+
